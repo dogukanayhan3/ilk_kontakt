bbcb78e86a787d7e26500b92691b543b9c8f5542
Doğukan Ayhan <ayhan03dgkn@gmail.com>
2025-05-18 18:41:11 +0300

    update migrations

--- IlkKontakt.Backend/src/IlkKontakt.Backend.Domain.Shared/UserProfiles/SkillProficiency.cs
+++ IlkKontakt.Backend/src/IlkKontakt.Backend.Domain.Shared/UserProfiles/SkillProficiency.cs
@@ -2,5 +2,9 @@ namespace IlkKontakt.Backend.UserProfiles;
 
 public enum SkillProficiency
 {
-    
+    One,
+    Two,
+    Three,
+    Four,
+    Five
 }
--- IlkKontakt.Backend/src/IlkKontakt.Backend.Domain/UserProfiles/Skill.cs
+++ IlkKontakt.Backend/src/IlkKontakt.Backend.Domain/UserProfiles/Skill.cs
@@ -1,6 +1,11 @@
+using System;
+using Volo.Abp.Domain.Entities.Auditing;
+
 namespace IlkKontakt.Backend.UserProfiles;
 
-public class Skill
+public class Skill : AuditedEntity<Guid>
 {
-    
+    public Guid ProfileId { get; set; }
+    public String SkillName { get; set; }
+    public SkillProficiency SkillProficiency { get; set; }
 }
