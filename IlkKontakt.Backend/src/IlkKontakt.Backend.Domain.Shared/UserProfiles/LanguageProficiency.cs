eb30aa1286aeb9b3ba0d413df4fe3bcf4c3df00e
Doğukan Ayhan <ayhan03dgkn@gmail.com>
2025-05-18 18:41:11 +0300

    update migrations

--- IlkKontakt.Backend/src/IlkKontakt.Backend.Domain.Shared/UserProfiles/LanguageProficiency.cs
+++ IlkKontakt.Backend/src/IlkKontakt.Backend.Domain.Shared/UserProfiles/LanguageProficiency.cs
@@ -2,5 +2,9 @@ namespace IlkKontakt.Backend.UserProfiles;
 
 public enum LanguageProficiency
 {
-    
+    Elementary,
+    LimitedWorking,
+    ProfessionalWorking,
+    Fluent,
+    Native
 }
--- IlkKontakt.Backend/src/IlkKontakt.Backend.Domain/UserProfiles/Project.cs
+++ IlkKontakt.Backend/src/IlkKontakt.Backend.Domain/UserProfiles/Project.cs
@@ -1,6 +1,14 @@
+using System;
+using Volo.Abp.Domain.Entities.Auditing;
+
 namespace IlkKontakt.Backend.UserProfiles;
 
-public class Project
+public class Project : AuditedEntity<Guid>
 {
-    
+    public Guid ProfileId { get; set; }
+    public String ProjectName { get; set; }
+    public String Description { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public String? ProjectUrl { get; set; }
 }
