267d99d7a7413fc31940aee4a9a3af1f76e2f18d
Doğukan Ayhan <113291789+dogukanayhan3@users.noreply.github.com>
2025-05-29 00:41:06 +0300

    Update ContactUsDto.cs

--- IlkKontakt.Backend/src/IlkKontakt.Backend.Application.Contracts/ContactUss/ContactUsDto.cs
+++ IlkKontakt.Backend/src/IlkKontakt.Backend.Application.Contracts/ContactUss/ContactUsDto.cs
@@ -5,7 +5,6 @@ namespace IlkKontakt.Backend.ContactUs;
 
 public class ContactUsDto : AuditedAggregateRoot<Guid>
 {
-    public Guid ContactId { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public string Message { get; set; }
